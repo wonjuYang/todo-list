030ab2a8d1c884142e4978937c4152b990dbe169
triplej <his1monologue@gmail.com>
2020-07-13 23:42:41 +0900

    PageTemplate 기초 만들기

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        일정 관리
+      </div>
+    )
+  }
+}
+export default App;
