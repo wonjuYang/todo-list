fab6bd416214d30a047c18c6cda1f022c0c683d7
triplej <his1monologue@gmail.com>
2020-07-14 18:55:26 +0900

    최적화 전까지 일단 완성

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,106 @@
 import React, {Component} from 'react';
 import PageTemplate from './PageTemplate';
-import TodoInput from './TodoInput/TodoInput.js';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
 
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 class App extends Component {
+  state = {
+    input: '', //input 값
+    //일정 데이터 초깃값
+    todos: [
+      {id: 0, text: '리액트 공부하기', done: true},
+      {id: 1, text: '컴포넌트 스타일링 해보기', done: false}
+    ]
+  }
+
+  //일정 데이터 안에 들어가는 id 값
+  id = 1
+  getId = () => {
+    return ++this.id;
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      input: value
+    });
+  }
+
+  //새 데이터 추가
+  handleInsert = () => {
+    const {todos, input} = this.state;
+
+    //새 데이터 객체 만들기
+    const newTodo = {
+      text:input,
+      done:false,
+      id:this.getId()
+    };
+
+    //배열 안에 새 데이터를 집어넣습니다.
+    this.setState({
+      todos: [...todos, newTodo],
+      input: ''
+    });
+  }
+
+    handleToggle = (id) => {
+      //id로 배열의 인덱스를 찾습니다
+      const {todos} = this.state;
+      const index = todos.findIndex(todo => todo.id === id);
+
+      const toggled = {
+        ...todos[index],
+        done: !todos[index].done
+      };
+
+      //slice를 사용하여 우리가 찾은 index 전호의 데이터들을 복사합니다.
+      //그리고 그 사이에는 변경된 todo 객체를 넣어줍니다
+
+      this.setState ({
+        todos:[
+          ...todos.slice(0, index),
+          toggled,
+          ...todos.slice(index +1, todos.length)
+        ]
+        
+      });
+    }
+
+    //선택한 id를 배열에서 제거합니다.
+    handleRemove = (id) => {
+      const {todos} = this.state;
+      const index = todos.findIndex(todo => todo.id === id);
+
+      this.setState({
+        todos: [
+          ...todos.slice(0, index),
+          ...todos.slice(index +1, todos.length)
+        ]
+      });
+    }
+
+
   render() {
+    const { input, todos } = this.state;
+    
+
+    const {
+      handleChange,
+      handleInsert,
+      handleToggle,
+      handleRemove
+    } = this;
+
+
     return (
       <PageTemplate>
-        <TodoInput/>
+        <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
+        <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
       </PageTemplate>
     );
   }
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -7,13 +7,18 @@ const cx = classNames.bind(styles);
 class TodoItem extends Component {
     render() {
 
+        //레퍼런스를 만든 것, 이렇게 하면 props를 사용할 때마다 this.props.onToggle처럼 매번 앞부분에 붙이는 this.props 생략 가능
         const {done, children, onToggle, onRemove} = this.props;
 
         return (
-            <div className={cx('todo-item')} onCLick={onToggle}>
+            <div className={cx('todo-item')} onClick={onToggle}>
                 <input className={cx('tick')} type="checkbox" checked={done} readOnly/>
                 <div className={cx('text', {done})}>{children}</div>
-                <div classNMae={cx('delete')} onClick={onRemove}>[지우기]</div>
+                <div className={cx('delete')} onClick={(e) => {
+                    onRemove();
+                    e.stopPropagation();
+                    }
+                }>[지우기]</div>
             </div>
         );
     }
