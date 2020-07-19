245178fdb1ae8b9979642667d48e2dcbe5c615fc
triplej <his1monologue@gmail.com>
2020-07-20 00:14:30 +0900

    리덕스 쉽게 사용하기 버전

--- src/components/App.js
+++ src/components/App.js
@@ -1,110 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PageTemplate from './PageTemplate';
-import TodoInput from './TodoInput';
-import TodoList from './TodoList';
+import TodoInputContainer from '../containers/TodoInputContainer';
+import TodoListContainer from '../containers/TodoListContainer';
 
-//import 'bootstrap/dist/css/bootstrap.min.css';
-
-const initialTodos = new Array(500).fill(0).map(
-  (foo, index) => ({id: index, text: `일정 ${index}`, done: false})
-)
 
 
 class App extends Component {
-  state = {
-    input: '', //input 값
-    //일정 데이터 초깃값
-    todos: initialTodos,
-  }
-
-  //일정 데이터 안에 들어가는 id 값
-  id = 1
-  getId = () => {
-    return ++this.id;
-  }
-
-  handleChange = (e) => {
-    const { value } = e.target;
-    this.setState({
-      input: value
-    });
-  }
-
-  //새 데이터 추가
-  handleInsert = () => {
-    const {todos, input} = this.state;
-
-    //새 데이터 객체 만들기
-    const newTodo = {
-      text:input,
-      done:false,
-      id:this.getId()
-    };
-
-    //배열 안에 새 데이터를 집어넣습니다.
-    this.setState({
-      todos: [...todos, newTodo],
-      input: ''
-    });
-  }
-
-    handleToggle = (id) => {
-      //id로 배열의 인덱스를 찾습니다
-      const {todos} = this.state;
-      const index = todos.findIndex(todo => todo.id === id);
-
-      const toggled = {
-        ...todos[index],
-        done: !todos[index].done
-      };
-
-      //slice를 사용하여 우리가 찾은 index 전호의 데이터들을 복사합니다.
-      //그리고 그 사이에는 변경된 todo 객체를 넣어줍니다
-
-      this.setState ({
-        todos:[
-          ...todos.slice(0, index),
-          toggled,
-          ...todos.slice(index +1, todos.length)
-        ]
-        
-      });
-    }
-
-    //선택한 id를 배열에서 제거합니다.
-    handleRemove = (id) => {
-      const {todos} = this.state;
-      const index = todos.findIndex(todo => todo.id === id);
-
-      this.setState({
-        todos: [
-          ...todos.slice(0, index),
-          ...todos.slice(index +1, todos.length)
-        ]
-      });
-    }
-
-
   render() {
-    const { input, todos } = this.state;
-    
-
-    const {
-      handleChange,
-      handleInsert,
-      handleToggle,
-      handleRemove
-    } = this;
-
-
     return (
       <PageTemplate>
-        <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
-        <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+        <TodoInputContainer/>
+        <TodoListContainer/>
       </PageTemplate>
     );
   }
 }
+
+
 
 
 export default App;
---
+++ src/containers/TodoInputContainer.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import TodoInput from '../components/TodoInput';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+//액션 함수들을 한꺼번에불러옵니다.
+import * as inputActions from '../modules/input';
+import * as todosActions from '../modules/todos';
+
+
+class TodoInputContainer extends Component {
+
+    id = 1
+    getId = () => {
+        return ++this.id;
+    }
+
+    handleChange = (e) => {
+        const {value} = e.target;
+        const {InputActions} = this.props;
+        InputActions.setInput(value);
+    }
+
+    handleInsert = () => {
+        const { InputActions, TodosActions, value} = this.props;
+        const todo = {
+            id: this.getId(),
+            text: value,
+            done: false
+        };
+        TodosActions.insert(todo);
+        InputActions.setInput('');
+    }
+
+    render() {
+        const { value } = this.props;
+        const { handleChange, handleInsert } = this;
+        return (
+            <TodoInput
+                onChange={handleChange}
+                onInsert={handleInsert}
+                value={value}
+            />
+        );
+    }
+}
+
+
+/* 이번에는 mapStateToProps와 mapDispatchToProps 함수에 대한 레퍼런스를
+따로 만들지 않고, 그 내부에 바로 정의해 주었습니다. */
+
+export default connect(
+    (state) => ({
+        value: state.input.get('value')
+    }),
+    (dispatch) => ({
+        InputActions: bindActionCreators(inputActions, dispatch),
+        TodosActions: bindActionCreators(todosActions, dispatch)
+    })
+)(TodoInputContainer);
+
