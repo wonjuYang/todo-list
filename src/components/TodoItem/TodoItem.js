25d930c0ffc5a9364fe5e3f1a41d1b94f5ea61c0
wonjuyang <his1monologue@gmail.com>
2020-09-04 17:58:45 +0900

    hit bug

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -5,29 +5,26 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 class TodoItem extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.done !== nextProps.done;
+  }
 
-    shouldComponentUpdate(nextProps, nextState){
-        return this.props.done !== nextProps.done;
-    }
-
-    
-    render() {
-
-        //레퍼런스를 만든 것, 이렇게 하면 props를 사용할 때마다 this.props.onToggle처럼 매번 앞부분에 붙이는 this.props 생략 가능
-        const {done, children, onToggle, onRemove} = this.props;
-
-        return (
-            <div className={cx('todo-item')} onClick={onToggle}>
-                <input className={cx('tick')} type="checkbox" checked={done} readOnly/>
-                <div className={cx('text', {done})}>{children}</div>
-                <div className={cx('delete')} onClick={(e) => {
-                    onRemove();
-                    e.stopPropagation();
-                    }
-                }>[지우기]</div>
-            </div>
-        );
-    }
+  render() {
+    const {done, children, onToggle, onRemove} = this.props;
+    /* 위 코드에선 비구조화 할당을 통하여 this.props 안에 있는
+       done, children, onToggle, onRemove 에 대한 레퍼런스를 만들어주었습니다. */
+    return (
+      <div className={cx('todo-item')} onClick={onToggle}>
+        <input className={cx('tick')} type="checkbox" checked={done} readOnly/>
+        <div className={cx('text', { done })}>{children}</div>
+        <div className={cx('delete')} onClick={(e) => {
+          onRemove();
+          e.stopPropagation();
+          }
+        }>[지우기]</div>
+      </div>
+    );
+  }
 }
 
 export default TodoItem;
--- src/modules/input.js
+++ src/modules/input.js
@@ -1,17 +1,17 @@
 import { Map } from 'immutable';
 import { handleActions, createAction } from 'redux-actions';
 
-const SET_INPUT = 'input/SET_INPUT';
 
+const SET_INPUT = 'input/SET_INPUT';
 
 export const setInput = createAction(SET_INPUT);
 
 const initialState = Map({
-    value: ''
-})
+  value: ''
+});
 
 export default handleActions({
-    [SET_INPUT]: (state, action) => {
-        return state.set('value', action.payload)
-    }
+  [SET_INPUT]: (state, action) => {
+    return state.set('value', action.payload)
+  }
 }, initialState);
