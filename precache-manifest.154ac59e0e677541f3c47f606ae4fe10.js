335e67e19d2d0a72f1e8142efff3405611202d84
wonjuyang <his1monologue@gmail.com>
2020-09-04 17:48:05 +0900

    Updates

---
+++ precache-manifest.154ac59e0e677541f3c47f606ae4fe10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0df6165fe42c1cf2e24f12aed7d394",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "a1219e99bf0a1a615b06",
+    "url": "/todo-list/static/css/main.7da86bfd.chunk.css"
+  },
+  {
+    "revision": "7502fe9d9215f0993ba4",
+    "url": "/todo-list/static/js/2.47aabfb6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todo-list/static/js/2.47aabfb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1219e99bf0a1a615b06",
+    "url": "/todo-list/static/js/main.87f2c5ae.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
